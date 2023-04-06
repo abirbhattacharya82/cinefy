696c1626e083524ac3c4030c05284010abed3bb7
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-06 03:32:19 +0000

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Background from './components/Background/Background';
+import Home from './components/Home/Home';
 function App() {
   return (
     <div className="App">
-      <Background/>
+      <Background />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Home.css';
+import logo from './logo.png';
+function Home(){
+    return(
+        <div className="Home">
+            <div className="Left">
+                <div className="Logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="Text">
+                    Cinefy is your one-stop destination for indulging in nostalgia with some of the best Indian movies from the 2000s era. Sign up now and dive into a world of free entertainment, with an extensive collection of classic Bollywood flicks waiting to be streamed.
+                </div>
+            </div>
+            <div className="Right">
+
+            </div>
+        </div>
+    )
+};
+export default Home;
