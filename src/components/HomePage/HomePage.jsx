b808a006564849ca36d76cb30af827b64f520c01
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-07 15:48:18 +0000

    Other devices

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
 import Background from '../Background/Background';
 import Home from '../Home/Home';
+import Other from '../Otherdevices/Otherdevices';
+import { useState, useEffect } from 'react';
 function HomePage() {
+  const websiteWidth = window.innerWidth;
+  const [showHome, setShowHome] =useState(false);
+  useEffect(()=>{
+    if(websiteWidth>=1000){
+      setShowHome(true);
+    }
+  },[websiteWidth])
   return (
     <div className="HomePage">
       <Background />
-      <Home />
+      {showHome? <Home /> : <Other /> }
     </div>
   );
 }
---
+++ src/components/Otherdevices/Otherdevices.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Otherdevices.css';
+function Otherdevices(){
+    return(
+        <div className="Otherdevices">
+
+        </div>
+    )
+};
+export default Otherdevices;
