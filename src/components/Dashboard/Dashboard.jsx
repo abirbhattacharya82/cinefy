6c47edf670c9234a0143a5116c526ca960cf4f2c
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-07 03:54:07 +0000

    Logout Functionalities

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import logo from './logo.png';
 import './Dashboard.css';
-function Dashboard(){
-    const [navId,setNavId]=useState(1);
-    return(
+function Dashboard() {
+    const [navId, setNavId] = useState(1);
+    function homeDashboard(){
+        setNavId(1);
+    }
+    function wishlistDashboard(){
+        setNavId(2);
+    }
+    function logOut(){
+        localStorage.token="";
+        window.location.href = '/';
+    }
+    return (
         <div className="Dashboard">
             <div className="Navbar">
-                <button id="logo">
+                <button id="logo" onClick={homeDashboard}>
                     <img src={logo} alt="" />
                 </button>
-                <button id="wishlist">Wishlist</button>
+                <button id="wishlist" onClick={wishlistDashboard}>Wishlist</button>
                 <div className="Gap"></div>
-                <button id="logout">LogOut</button>
+                <button id="logout" onClick={logOut}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                </button>
             </div>
         </div>
     )
