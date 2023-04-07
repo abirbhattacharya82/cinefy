ef45d62d350fc4bc6e49ca686bbdf81df4b8ff66
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-07 15:58:23 +0000

    Mobile responsive managed

--- src/components/Otherdevices/Otherdevices.jsx
+++ src/components/Otherdevices/Otherdevices.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './Otherdevices.css';
+import Sorry from './sorry.png';
 function Otherdevices(){
     return(
         <div className="Otherdevices">
-
+            <div className="Image">
+                <img src={Sorry} alt="" />
+            </div>
         </div>
     )
 };
