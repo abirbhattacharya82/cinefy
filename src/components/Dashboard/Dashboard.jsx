44d5b26433424f996762331acc6b0c8e2da261c9
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-07 03:39:17 +0000

    before implementing logout button

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import logo from './logo.png';
+import './Dashboard.css';
 function Dashboard(){
+    const [navId,setNavId]=useState(1);
     return(
         <div className="Dashboard">
-            Dashboard
+            <div className="Navbar">
+                <button id="logo">
+                    <img src={logo} alt="" />
+                </button>
+                <button id="wishlist">Wishlist</button>
+                <div className="Gap"></div>
+                <button id="logout">LogOut</button>
+            </div>
         </div>
     )
 };
